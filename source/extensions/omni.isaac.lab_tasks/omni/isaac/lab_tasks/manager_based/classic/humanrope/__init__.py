430f7e3cb8f4568c121bf5f3539af3929fa264a9
Shengchao Yan <yan@informatik.uni-freiburg.de>
2024-09-06 09:47:08 +0200

    [humanrope] add task of humanoid walking on tight rope

---
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/classic/humanrope/__init__.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Humanoid locomotion on tight rope environment (similar to OpenAI Gym Humanoid-v2).
+"""
+
+import gymnasium as gym
+
+from . import agents, humanrope_env_cfg
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="Isaac-Humanrope-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": humanrope_env_cfg.HumanropeEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumanropePPORunnerCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
+    },
+)
