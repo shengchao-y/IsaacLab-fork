7bb7a058f749f8d711d770763e4123b0fa1bab86
Shengchao Yan <yan@informatik.uni-freiburg.de>
2025-01-02 16:21:11 +0100

    [antonball] fix config errors

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/classic/antonball/agents/rsl_rl_ppo_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/classic/antonball/agents/rsl_rl_ppo_cfg.py
@@ -9,9 +9,6 @@ from omni.isaac.lab_tasks.utils.wrappers.rsl_rl import (
     RslRlOnPolicyRunnerCfg,
     RslRlPpoActorCriticCfg,
     RslRlPpoAlgorithmCfg,
-    RslRlOffPolicyRunnerCfg,
-    RslRlSacActorCriticCfg,
-    RslRlSacAlgorithmCfg,
 )
 
 
@@ -23,13 +20,14 @@ class AntonballPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         # "rew_alive": 2.0,
     }
     gage_init_std = 0.0
+    gage_change_rate = 0.0
     num_steps_per_env = 32
-    max_iterations = 6000
-    save_interval = 50
+    max_iterations = 5000
+    save_interval = 100
     experiment_name = "antonball"
     empirical_normalization = False
     policy = RslRlPpoActorCriticCfg(
-        class_name="ActorCritic_RND",
+        class_name="ActorCritic",
         init_noise_std=1.0,
         actor_hidden_dims=[400, 200, 100],
         critic_hidden_dims=[400, 200, 100],
@@ -48,37 +46,4 @@ class AntonballPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         lam=0.95,
         desired_kl=0.01,
         max_grad_norm=1.0,
-    )
-
-@configclass
-class AntonballSACRunnerCfg(RslRlOffPolicyRunnerCfg):
-    rewards_expect = {
-        "rew_pole_moving": 1.0,
-        # "rew_alive": 2.0,
-    }
-    num_steps_per_env = 32
-    max_iterations = 6000
-    save_interval = 50
-    experiment_name = "antonball"
-    capacity_per_env = 300
-    policy = RslRlSacActorCriticCfg(
-        actor_hidden_dims=[400, 200, 100],
-        critic_hidden_dims=[400, 200, 100],
-        activation="elu",
-        log_std_min=-5.0,
-        log_std_max=5.0,
-        use_layer_norm=False,
-    )
-    algorithm = RslRlSacAlgorithmCfg(
-        alpha=1.0,
-        num_learning_epochs=5,
-        critic_lr=5.0e-4,
-        actor_lr=5.0e-4,
-        alpha_lr=5.0e-4,
-        gamma=0.99,
-        batch_size=8192,
-        max_grad_norm=1.0,
-        target_entropy=-10.0,
-        tau=0.005,
-        empirical_normalization=True,
     )
